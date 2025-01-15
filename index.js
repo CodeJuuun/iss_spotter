de0b2b6fae9824647a3bd123180e707511c38f31
CodeJuuun <Chomseng.a@gmail.com>
2025-01-15 17:17:09 -0500

    implemented a fetchCoordsByIP function to grab IP from API endpoint

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 // // main fetch function
-// const {fetchMyIP} = require("./iss");
+const {fetchMyIP} = require("./iss");
+// const {fetchCoordsByIP} = require("./iss");
 
-// fetchMyIP((error, ip) => {
-//   if (error) { // checks if error is truthy
-//     console.log("It didn't work!", error);
+//-------------------------------------------
+const ip = "64.228.242.100";
+fetchMyIP((error, ip) => {
+  if (error) { // checks if error is truthy
+    console.log("It didn't work!", error);
+    return;
+  }
+
+  console.log("It worked! Returned IP: ", ip);
+});
+
+//-------------------------------------------
+// fetchCoordsByIP(ip, (error, data) => {
+//   if (error) {
+//     console.log("It did not work!", error);
 //     return;
 //   }
 
-//   console.log("It worked! Returned IP: ", ip);
+//   console.log("It worked. Received data:" , data);
 // });
--- iss.js
+++ iss.js
@@ -11,25 +11,63 @@ const needle = require("needle");
  *   - The IP address as a string (null if error). Example: "162.245.144.188"
  */
 const fetchMyIP = function(callback) {
-  const myIP = "https://api.ipify.org?format=json";
+  const url = "https://api.ipify.org?format=json";
   // console.log(myIP)
 
-  needle.get(myIP, (error, res, body) => {
+  needle.get(url, (error, res, body) => { // Make a GET request to the IP
     // code will be used to check for errors
     // first set of checks before we try to parse body
     if (error) {
-      return callback(error, null);
+      return callback(error, null); // return error and stop running code
     }
 
-    if (res.statusCode !== 200) {
+    if (res.statusCode !== 200) { // check if server responded with a non 200 code
       const msg = `Status code ${res.statusCode} when fetching IP. Response: ${body}`;
       callback(Error(msg), null);
       return;
     }
 
     const ip = body.ip;
-    callback(null, ip)
+    callback(null, ip);
   });
 };
+//----------------------------------------------------------
+/**
+ * @param {string} ip
+ */
+const fetchCoordsByIP = (ip, callback) => {
+  const url = `http://ipwho.is/${ip}`;
 
-module.exports = { fetchMyIP }; //curly braces means it's exporting more than one value as a single object, or possibly more values later
+  needle.get(url, (error, res, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    
+    // first check if the body data is a string, if so, convert JSON data into object via json.parse()
+    let jsonParsedBody;
+    if (typeof body === 'string') {
+      jsonParsedBody = JSON.parse(body);
+    } else {
+      jsonParsedBody = body; // if its already an object, just use it as is
+    }
+
+    if (!jsonParsedBody.success) {
+      const msg = `Success status was ${jsonParsedBody.success}. Server message says: ${jsonParsedBody.message} when fetching for IP ${jsonParsedBody.ip}`;
+      callback(msg, null); // Return the message directly as a string
+      return;
+    }
+    // check if longitude and latitude even exists in the response via an object and extract it
+    if (jsonParsedBody.latitude && jsonParsedBody.longitude) {
+      const coordinates = {
+        latitude: jsonParsedBody.latitude,
+        longitude: jsonParsedBody.longitude
+      };
+      callback(null, coordinates); // passing back
+    } else {
+      callback("Coordinates not found", null);
+    }
+  });
+};
+module.exports = { fetchMyIP, fetchCoordsByIP }; //curly braces means it's exporting more than one value as a single object, or possibly more values later
+
